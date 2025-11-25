58c7674008510a298a8f797cce6cc6756e973cf9
hanumanmule487 <168597318+hanumanmule487@users.noreply.github.com>
2025-11-25 14:42:13 +0530

    Change root component

---
+++ src/login/login.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { AppComponent } from '../app/app.component';
+
+@Component({
+  selector: 'app-login',
+  imports: [AppComponent],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.css'
+})
+export class LoginComponent {
+
+}
